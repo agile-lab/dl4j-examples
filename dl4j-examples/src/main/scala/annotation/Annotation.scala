a53f8f3d0f2326125c42a1e9381b51e0ec9a9a21
paolo <paolo.platter@agilelab.it>
2017-05-24 15:30:59 +0200

    annotation

--- dl4j-examples/src/main/scala/annotation/Annotation.scala
+++ dl4j-examples/src/main/scala/annotation/Annotation.scala
@@ -1,8 +1,30 @@
 package annotation
 
+import java.io.{DataInput, DataOutput}
+
+import org.datavec.api.writable.{ArrayWritable, Writable}
+
 /**
   * Created by paolo on 24/05/2017.
   */
-class Annotation {
+trait Annotation {
 
+}
+
+
+class MultiNDArrayWritable(val list: List[Writable]) extends ArrayWritable {
+
+  override def getDouble(i: Long): Double = throw new UnsupportedOperationException
+
+  override def length(): Long = throw new UnsupportedOperationException
+
+  override def getFloat(i: Long): Float = throw new UnsupportedOperationException
+
+  override def getLong(i: Long): Long = throw new UnsupportedOperationException
+
+  override def getInt(i: Long): Int = throw new UnsupportedOperationException
+
+  override def write(out: DataOutput): Unit = throw new UnsupportedOperationException
+
+  override def readFields(in: DataInput): Unit = throw new UnsupportedOperationException
 }
