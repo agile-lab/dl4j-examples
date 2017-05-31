d6135c95b70a67e0821f84d2722e586e1ccde556
paolo <paolo.platter@agilelab.it>
2017-05-31 15:13:51 +0200

    fix

--- dl4j-examples/src/main/scala/deeplogo/Main.scala
+++ dl4j-examples/src/main/scala/deeplogo/Main.scala
@@ -10,7 +10,7 @@ object Main extends App {
   val conf = new ConfigurationImpl()
   val net = new SimpleNet(conf).createNet()
 
-  //new LogoClassification(net, conf).exec()
+  new LogoClassification(net, conf).exec()
 
-  new Test(conf).regionTest()
+  //new Test(conf).regionTest()
 }
