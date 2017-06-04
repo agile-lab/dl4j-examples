a687fb9522e3bbadce31467257d7deb6d45b090b
paolo <paolo.platter@agilelab.it>
2017-06-04 13:19:07 +0200

    dd

--- dl4j-examples/src/main/java/org/deeplearning4j/examples/convolution/Vgg16.java
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/convolution/Vgg16.java
@@ -58,7 +58,7 @@ public class Vgg16 {
                 .updater(Updater.NESTEROVS)
                 .activation(Activation.RELU)
                 .learningRate(0.002)
-                //.regularization(true).l2(1e-4)
+                .regularization(true).l2(1e-4)
                 .momentum(0.9)
                 .list()
 
