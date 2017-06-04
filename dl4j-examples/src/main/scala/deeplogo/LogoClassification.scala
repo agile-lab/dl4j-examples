415c1fb4b0b082c6623d338f7e770b986fddeaca
paolo <paolo.platter@agilelab.it>
2017-06-05 01:10:45 +0200

    dd

--- dl4j-examples/src/main/scala/deeplogo/LogoClassification.scala
+++ dl4j-examples/src/main/scala/deeplogo/LogoClassification.scala
@@ -122,7 +122,7 @@ class LogoClassification(val network: MultiLayerNetwork, conf: Configuration) {
         eval = network.evaluate(testIter)
         System.out.println(eval.stats)
         testIter.reset()
-        if(eval.accuracy() > 0.7)
+        if(eval.accuracy() > 0.85)
           force=true
 
       }
