62783b41fb85e74a033b0c691a46d92432d0e6d1
paolo <paolo.platter@agilelab.it>
2017-06-14 18:30:27 +0200

    googlenet

--- dl4j-examples/src/main/java/org/deeplearning4j/examples/transferlearning/vgg16/TransferGoogleNet.java
+++ dl4j-examples/src/main/java/org/deeplearning4j/examples/transferlearning/vgg16/TransferGoogleNet.java
@@ -1,7 +1,6 @@
 package org.deeplearning4j.examples.transferlearning.vgg16;
 
 import lombok.extern.slf4j.Slf4j;
-import org.datavec.api.io.filters.BalancedPathFilter;
 import org.datavec.api.io.filters.RandomPathFilter;
 import org.datavec.api.io.labels.ParentPathLabelGenerator;
 import org.datavec.api.split.FileSplit;
@@ -11,23 +10,18 @@ import org.datavec.image.recordreader.ImageRecordReader;
 import org.deeplearning4j.datasets.datavec.RecordReaderDataSetIterator;
 import org.deeplearning4j.datasets.iterator.MultipleEpochsIterator;
 import org.deeplearning4j.eval.Evaluation;
-import org.deeplearning4j.examples.transferlearning.vgg16.dataHelpers.FlowerDataSetIterator;
+import org.deeplearning4j.nn.api.Model;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
-import org.deeplearning4j.nn.conf.InputPreProcessor;
 import org.deeplearning4j.nn.conf.Updater;
 import org.deeplearning4j.nn.conf.distribution.GaussianDistribution;
 import org.deeplearning4j.nn.conf.distribution.NormalDistribution;
-import org.deeplearning4j.nn.conf.inputs.InputType;
 import org.deeplearning4j.nn.conf.layers.DenseLayer;
 import org.deeplearning4j.nn.conf.layers.OutputLayer;
-import org.deeplearning4j.nn.conf.preprocessor.CnnToFeedForwardPreProcessor;
 import org.deeplearning4j.nn.graph.ComputationGraph;
 import org.deeplearning4j.nn.modelimport.keras.InvalidKerasConfigurationException;
-import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
-import org.deeplearning4j.nn.modelimport.keras.Model;
 import org.deeplearning4j.nn.modelimport.keras.UnsupportedKerasConfigurationException;
 import org.deeplearning4j.nn.modelimport.keras.trainedmodels.TrainedModelHelper;
-import org.deeplearning4j.nn.modelimport.keras.trainedmodels.TrainedModels;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.transferlearning.FineTuneConfiguration;
 import org.deeplearning4j.nn.transferlearning.TransferLearning;
 import org.deeplearning4j.nn.weights.WeightInit;
@@ -35,8 +29,6 @@ import org.deeplearning4j.util.ModelSerializer;
 import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.api.iterator.DataSetIterator;
-import org.nd4j.linalg.dataset.api.preprocessor.DataNormalization;
-import org.nd4j.linalg.dataset.api.preprocessor.ImagePreProcessingScaler;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 
 import java.io.File;
@@ -60,27 +52,22 @@ import static org.deeplearning4j.nn.modelimport.keras.trainedmodels.TrainedModel
  * When running multiple epochs this can save on computation time.
  */
 @Slf4j
-public class EditLastLayerOthersFrozen {
+public class TransferGoogleNet {
 
-    protected static final int numLabels = 7;
+    protected static final int numLabels = 8;
     protected static final long seed = 12345;
 
     //private static final int trainPerc = 80;
     private static final int batchSize = 10;
     private static final String featureExtractionLayer = "fc2";
 
-
     public static void main(String [] args) throws UnsupportedKerasConfigurationException, IOException, InvalidKerasConfigurationException {
 
-        //Import vgg
-        //Note that the model imported does not have an output layer (check printed summary)
-        //  nor any training related configs (model from keras was imported with only weights and json)
-        TrainedModelHelper modelImportHelper = new TrainedModelHelper(VGG16);
-        System.out.println("\n\nLoading vgg16...\n\n");
-        ComputationGraph vgg16 = modelImportHelper.loadModel();
-        System.out.println(vgg16.summary());
-        int height = 224;
-        int width = 224;
+
+
+        File modelFile = new File("C:\\Users\\paolo\\Documents\\data\\googlenet_dl4j_inference.zip");
+        MultiLayerNetwork model = ModelSerializer.restoreMultiLayerNetwork(modelFile);
+        System.out.println(model.summary());
 
 
         //KerasModelImport.importKerasModelAndWeights(, "C:\\Users\\agilelab\\.dl4j\\trainedmodels\\resnet50\\resnet50_weights_th_dim_ordering_th_kernels.h5", false);
@@ -100,42 +87,27 @@ public class EditLastLayerOthersFrozen {
             .build();
 
         //Construct a new model with the intended architecture and print summary
-        ComputationGraph vgg16Transfer = new TransferLearning.GraphBuilder(vgg16)
+        MultiLayerNetwork googleNetTransfer = new TransferLearning.Builder(model)
             .fineTuneConfiguration(fineTuneConf)
-            .setFeatureExtractor("block3_pool") //the specified layer and below are "frozen"
-            .removeVertexAndConnections("block4_conv1")
-            .removeVertexAndConnections("block4_conv2")
-            .removeVertexAndConnections("block4_conv3")
-            .removeVertexAndConnections("block4_pool")
-            .removeVertexAndConnections("block5_conv1")
-            .removeVertexAndConnections("block5_conv2")
-            .removeVertexAndConnections("block5_conv3")
-            .removeVertexAndConnections("block5_pool")
-            .removeVertexAndConnections("flatten")
-            .removeVertexAndConnections("fc1")
-            .removeVertexAndConnections("fc2")
-            .removeVertexAndConnections("predictions")
-            //.removeVertexKeepConnections("predictions") //replace the functionality of the final vertex
-
-            .addLayer("myff", new DenseLayer.Builder().name("ffn2").nIn(200704).nOut(2048).biasInit(0).dropOut(0.5).dist(new GaussianDistribution(0, 0.01)).build(), new CnnToFeedForwardPreProcessor(), "block3_pool")
-            .addLayer("predictions",
+            .setFeatureExtractor(model.getnLayers()-2) //the specified layer and below are "frozen"
+            .removeLayersFromOutput(2)
+            .addLayer(new DenseLayer.Builder().name("ffn2").nOut(1024).biasInit(0).dropOut(0.5).dist(new GaussianDistribution(0, 0.01)).build())
+            .addLayer(
                 new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
-                    .nIn(2048).nOut(numLabels)
+                    .nIn(1024).nOut(numLabels)
                     .weightInit(WeightInit.DISTRIBUTION)
                     .dist(new NormalDistribution(0,0.2*(2.0/(4096+numLabels)))) //This weight init dist gave better results than Xavier
-                    .activation(Activation.SOFTMAX).build(),
-                "myff")
-            .setOutputs("predictions")
-
+                    .activation(Activation.SOFTMAX).build())
             .build();
-        System.out.println(vgg16Transfer.summary());
+        System.out.println(googleNetTransfer.summary());
 
 
         //------------------------------------------------------
 
-        int numExamples = 700;
+        int numExamples = 970;
         double splitTrainTest= 0.8;
-
+        int height = 224;
+        int width = 224;
         int channels = 3;
         ParentPathLabelGenerator labelMaker = new ParentPathLabelGenerator();
         Random rng = new Random(43);
@@ -144,8 +116,8 @@ public class EditLastLayerOthersFrozen {
         int ncores = 6;
 
 
-        File mainPath = new File("C:\\Users\\paolo\\Documents\\data\\MyLogosCropped100");
-        File testPath = new File("C:\\Users\\paolo\\Documents\\data\\Flickr32_7classes");
+        File mainPath = new File("C:\\Users\\agilelab\\Documents\\MyLogosCropped100");
+        File testPath = new File("C:\\Users\\agilelab\\Documents\\Flickr32_7classes");
 
         FileSplit fileSplit = new FileSplit(mainPath, NativeImageLoader.ALLOWED_FORMATS, rng);
         FileSplit fileSplitTest = new FileSplit(testPath, NativeImageLoader.ALLOWED_FORMATS, rng);
@@ -189,27 +161,27 @@ public class EditLastLayerOthersFrozen {
         //DataSetIterator testIter = FlowerDataSetIterator.testIterator();
 
         Evaluation eval;
-        eval = vgg16Transfer.evaluate(testIter);
+        eval = googleNetTransfer.evaluate(testIter);
         System.out.println("Eval stats BEFORE fit on Good Test.....");
         System.out.println(eval.stats() + "\n");
         testIter.reset();
 
-        eval = vgg16Transfer.evaluate(testIter2);
+        eval = googleNetTransfer.evaluate(testIter2);
         System.out.println("Eval stats BEFORE fit on Bad Test.....");
         System.out.println(eval.stats() + "\n");
         testIter2.reset();
 
         int iter = 0;
         while(trainMIter.hasNext()) {
-            vgg16Transfer.fit(trainMIter.next());
+            googleNetTransfer.fit(trainMIter.next());
             if (iter % 10 == 0) {
                 System.out.println("Evaluate model at iter "+iter +" .... Good Test");
-                eval = vgg16Transfer.evaluate(testIter);
+                eval = googleNetTransfer.evaluate(testIter);
                 System.out.println(eval.stats());
                 testIter.reset();
 
                 System.out.println("Evaluate model at iter "+iter +" .... Bad Test");
-                eval = vgg16Transfer.evaluate(testIter2);
+                eval = googleNetTransfer.evaluate(testIter2);
                 System.out.println(eval.stats());
                 testIter2.reset();
             }
@@ -218,9 +190,9 @@ public class EditLastLayerOthersFrozen {
 
         System.out.println("Model build complete");
 
-        File locationToSave = new File("MyLogoGraph.zip");
+        File locationToSave = new File("MyLogoGraphGoogleNet.zip");
         boolean saveUpdater = false;
-        ModelSerializer.writeModel(vgg16Transfer, locationToSave, saveUpdater);
+        ModelSerializer.writeModel(googleNetTransfer, locationToSave, saveUpdater);
 
         System.out.println("Model saved");
 
@@ -229,7 +201,7 @@ public class EditLastLayerOthersFrozen {
             INDArray features = testIter2.next().getFeatures();
             long start = System.currentTimeMillis();
 
-            INDArray out = vgg16Transfer.outputSingle(features);
+            INDArray out = googleNetTransfer.output(features);
 
             long end = System.currentTimeMillis();
 
